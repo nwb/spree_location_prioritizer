006a05231fbbf3cb031d0527bab44b5461e98faa
Albert Liu <albertliu@naturalwellbeing.com>
2015-07-28 16:06:16 -0700

    routing to item location update

--- app/models/spree/stock/prioritizer_decorator.rb
+++ app/models/spree/stock/prioritizer_decorator.rb
@@ -9,8 +9,8 @@ module Spree
         # nwb={"ca" => 10000}             in string "{\"gb\":10000}"
         # p=[de,nwb]
         # p.sort!{|t,y| y[o["country"]]<=>t[o["country"]]}  # t y sequence reversed here  p
-
-        packages.sort!{|f,s| (JSON.parse(s.stock_location.priorities)[inventory_units.first.order.ship_address.country.iso.downcase] || JSON.parse(s.stock_location.priorities)["all"] || 0) <=> (JSON.parse(f.stock_location.priorities)[inventory_units.first.order.ship_address.country.iso.downcase]  || JSON.parse(f.stock_location.priorities)["all"] || 0)}
+        address= inventory_units.first.order.ship_address || inventory_units.first.order.billing_address
+        packages.sort!{|f,s| (JSON.parse(s.stock_location.priorities)[address.country.iso.downcase] || JSON.parse(s.stock_location.priorities)["all"] || 0) <=> (JSON.parse(f.stock_location.priorities)[address.country.iso.downcase]  || JSON.parse(f.stock_location.priorities)["all"] || 0)}
 
         Rails.logger.info("packages after sort: #{packages.length} at locations of #{packages.map(&:stock_location).inspect}")
         packages
